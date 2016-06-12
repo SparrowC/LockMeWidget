87f4d19d9f5b6010b1110966f9e0b2e3f71967cb
jiangkun <jiangkun@xiaomi.com>
2016-06-12 14:17:54 +0800

    finish view

---
+++ app/src/main/java/com/kun/lockmewidget/LockPatternUtil.java
@@ -0,0 +1,22 @@
+package com.kun.lockmewidget;
+
+/**
+ * Created by Kun on 16-6-12.
+ */
+public class LockPatternUtil {
+    public static boolean isInCircle(LockPatternView.Point point, int mRadius, float x, float y) {
+        return Math.pow(point.getX() - x, 2) + Math.pow(point.getY() - y, 2) < Math.pow(mRadius, 2);
+    }
+
+    public static float getDistanceBetweenTwoPoints(float fpX, float fpY, float spX, float spY) {
+        return (float) Math.sqrt((spX - fpX) * (spX - fpX) + (spY - fpY) * (spY - fpY));
+    }
+
+    public static float getAngleLineIntersectX(float fpX, float fpY, float spX, float spY, float distance) {
+        return (float) Math.toDegrees(Math.acos((spX - fpX) / distance));
+    }
+
+    public static float getAngleLineIntersectY(float fpX, float fpY, float spX, float spY, float distance) {
+        return (float) Math.toDegrees(Math.acos((spY - fpY) / distance));
+    }
+}
--- app/src/main/java/com/kun/lockmewidget/LockPatternView.java
+++ app/src/main/java/com/kun/lockmewidget/LockPatternView.java
@@ -1,12 +1,16 @@
 package com.kun.lockmewidget;
 
 import android.content.Context;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
+import android.graphics.Matrix;
 import android.graphics.Paint;
+import android.graphics.Path;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
 import android.view.View;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by kun on 16-6-8.
@@ -14,11 +18,16 @@ import android.view.View;
 public class LockPatternView extends View {
     final private int size = 3;
     private Point[][] points = new Point[size][size];
-    private Bitmap pointNormal, pointPressed, pointError;
-    private boolean isInitPoints;
-    private int width, height;
-    private int offsetY, offsetX;
-    private Paint paint;
+    private List<Point> selectPoints;
+    private Paint defaultPaint, selectPaint, errorPaint;
+    private Path mPath;
+    private int mWidth, mHeight, mRadius, mInnerRadius;
+    private int offsetX = 10;
+    private int offsetY;
+    private int mBoxWidth;
+    private boolean isActionMove, isActionDown, isActionUp;
+    private OnLockPattern patterListener;
+    private static final double CONSTANT_COS_30 = Math.cos(Math.toRadians(30));
 
     public LockPatternView(Context context) {
         this(context, null);
@@ -34,107 +43,277 @@ public class LockPatternView extends View {
     }
 
     private void init(Context context, AttributeSet attrs, int defStyleAttr) {
-
+        initPaints();
+        mPath = new Path();
     }
 
-    private void initPoint() {
-        width = getMeasuredWidth();
-        height = getMeasuredHeight();
-        int spacing;
-        if (width < height) {
-            //竖屏
-            spacing = width / 4;
-            offsetY = (height - width) / 2 + spacing / 2;
-            offsetX = spacing / 2;
-        } else {
-            //横屏
-            spacing = height / 4;
-            offsetX = (width - height) / 2 + spacing / 2;
-            offsetY = spacing / 2;
-        }
-        width = height;
+    private void initPaints() {
+        defaultPaint = new Paint();
+        defaultPaint.setColor(getResources().getColor(R.color.blue_78d2f6));
+        defaultPaint.setStrokeWidth(2.0f);
+        defaultPaint.setStyle(Paint.Style.STROKE);
+        defaultPaint.setAntiAlias(true);
 
+        selectPaint = new Paint();
+        selectPaint.setColor(getResources().getColor(R.color.blue_00aaee));
+        selectPaint.setStrokeWidth(4.0f);
+        selectPaint.setStyle(Paint.Style.STROKE);
+        selectPaint.setAntiAlias(true);
 
-        final BitmapFactory.Options options = new BitmapFactory.Options();
-
-        // Calculate inSampleSize
-        options.inSampleSize = 2;
-
-        // Decode bitmap with inSampleSize set
-        options.inJustDecodeBounds = false;
-
-        pointNormal = BitmapFactory.decodeResource(getResources(), R.drawable.locus_round_original, options);
-        pointPressed = BitmapFactory.decodeResource(getResources(), R.drawable.locus_round_click, options);
-        pointError = BitmapFactory.decodeResource(getResources(), R.drawable.locus_round_click_error, options);
-        paint = new Paint(Paint.ANTI_ALIAS_FLAG);
-
-        offsetX += pointNormal.getWidth() / 2;
-        for (int i = 0; i < size; i++) {
-            for (int j = 0; j < size; j++) {
-                points[i][j] = new Point(offsetX + spacing * i, offsetY + spacing * j);
-            }
-        }
+        errorPaint = new Paint();
+        errorPaint.setColor(getResources().getColor(R.color.red_f3323b));
+        errorPaint.setStrokeWidth(4.0f);
+        errorPaint.setStyle(Paint.Style.STROKE);
+        errorPaint.setAntiAlias(true);
     }
 
-    private void points2Canvas(Canvas canvas) {
-        for (int i = 0; i < size; i++) {
-            for (int j = 0; j < size; j++) {
-                switch (points[i][j].state) {
-                    case Point.POINT_NORMAL:
-                        canvas.drawBitmap(pointNormal, points[i][j].x, points[i][j].y, paint);
-                        break;
-                    case Point.POINT_PRESSED:
-                        canvas.drawBitmap(pointPressed, points[i][j].x, points[i][j].y, paint);
-                        break;
-                    case Point.POINT_ERROR:
-                        canvas.drawBitmap(pointError, points[i][j].x, points[i][j].y, paint);
-                        break;
-                }
+    private void initPoints() {
+        selectPoints = new ArrayList<>();
+        for (int i = 0; i < points.length; i++) {
+            for (int j = 0; j < points[0].length; j++) {
+                points[i][j] = new Point(offsetX + mBoxWidth / 2 + j * mBoxWidth, offsetY + mBoxWidth / 2 + i * mBoxWidth,
+                        i, j, i * points[0].length + j, Point.POINT_NORMAL);
             }
         }
     }
 
     @Override
-    protected void onDraw(Canvas canvas) {
-        super.onDraw(canvas);
-        if (!isInitPoints) {
-            isInitPoints = true;
-            initPoint();
-        }
-        points2Canvas(canvas);
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        mWidth = getMeasuredWidth();
+        mHeight = getMeasuredHeight();
+        mRadius = (mWidth - offsetX * 2) / 4 / 2 - offsetX;
+        mInnerRadius = mRadius / 3;
+        offsetY = (mHeight - mWidth) / 2;
+        mBoxWidth = (mWidth - offsetX * 2) / 3;
+        initPoints();
     }
 
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        drawToCanvas(canvas);
+    }
 
-    public int calculateInSampleSize(BitmapFactory.Options options,
-                                     int reqWidth, int reqHeight) {
-        // Raw height and width of image
-        final int height = options.outHeight;
-        final int width = options.outWidth;
-        int inSampleSize = 1;
+    private void drawToCanvas(Canvas canvas) {
+        for (int i = 0; i < points.length; i++) {
+            for (int j = 0; j < points[0].length; j++) {
+                switch (points[i][j].getStatus()) {
+                    case Point.POINT_NORMAL:
+                        canvas.drawCircle(points[i][j].getX(), points[i][j].getY(), mRadius, defaultPaint);
+                        break;
+                    case Point.POINT_PRESSED:
+                        selectPaint.setStyle(Paint.Style.STROKE);
+                        canvas.drawCircle(points[i][j].getX(), points[i][j].getY(), mRadius, selectPaint);
+                        selectPaint.setStyle(Paint.Style.FILL);
+                        canvas.drawCircle(points[i][j].getX(), points[i][j].getY(), mInnerRadius, selectPaint);
+                        break;
+                    case Point.POINT_ERROR:
+                        errorPaint.setStyle(Paint.Style.STROKE);
+                        canvas.drawCircle(points[i][j].getX(), points[i][j].getY(), mRadius, errorPaint);
+                        errorPaint.setStyle(Paint.Style.FILL);
+                        canvas.drawCircle(points[i][j].getX(), points[i][j].getY(), mInnerRadius, errorPaint);
+                        break;
+                }
+            }
+        }
+        if (!selectPoints.isEmpty()) {
+            Point tempPoint = selectPoints.get(0);
+            for (int i = 1; i < selectPoints.size(); i++) {
+                Point point = selectPoints.get(i);
+                if (point.getStatus() == Point.POINT_PRESSED) {
+                    drawLine(tempPoint, point, canvas, selectPaint);
+                    drawTriangle(tempPoint, point, canvas, selectPaint);
+                } else if (point.getStatus() == Point.POINT_ERROR) {
+                    drawLine(tempPoint, point, canvas, errorPaint);
+                    drawTriangle(tempPoint, point, canvas, errorPaint);
+                }
+                tempPoint = point;
+            }
+        }
+    }
 
-        //先根据宽度进行缩小
-        while (width / inSampleSize > reqWidth) {
-            inSampleSize++;
+    private void drawTriangle(Point prePoint, Point nextPoint, Canvas canvas, Paint paint) {
+        float distance = LockPatternUtil.getDistanceBetweenTwoPoints
+                (prePoint.getX(), prePoint.getY(), nextPoint.getX(), nextPoint.getY());
+        float x = prePoint.getX();
+        float y = prePoint.getY() - this.mInnerRadius * 2;
+
+        float x1 = x - this.mInnerRadius / 2;
+        float y1 = y + (float) (this.mInnerRadius * CONSTANT_COS_30);
+        float x2 = x + this.mInnerRadius / 2;
+        float y2 = y1;
+
+        float angleX = LockPatternUtil.getAngleLineIntersectX(
+                prePoint.getX(), prePoint.getY(), nextPoint.getX(), nextPoint.getY(), distance);
+        float angleY = LockPatternUtil.getAngleLineIntersectY(
+                prePoint.getX(), prePoint.getY(), nextPoint.getX(), nextPoint.getY(), distance);
+
+        mPath.reset();
+        mPath.moveTo(x, y);
+        mPath.lineTo(x1, y1);
+        mPath.lineTo(x2, y2);
+        mPath.close();
+        //slide right down and right up
+        Matrix triangleMatrix = new Matrix();
+        if (angleX >= 0 && angleX <= 90) {
+            triangleMatrix.setRotate(180 - angleY, prePoint.getX(), prePoint.getY());
         }
-        //然后根据高度进行缩小
-        while (height / inSampleSize > reqHeight) {
-            inSampleSize++;
+        //slide left up and left down
+        else {
+            triangleMatrix.setRotate(angleY - 180, prePoint.getX(), prePoint.getY());
         }
-        return inSampleSize;
+        mPath.transform(triangleMatrix);
+        canvas.drawPath(mPath, paint);
+    }
+
+    private void drawLine(Point prePoint, Point nextPoint, Canvas canvas, Paint paint) {
+        canvas.drawLine(prePoint.getX(), prePoint.getY(),nextPoint.getX(), nextPoint.getY(),paint);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        int action = event.getAction();
+        switch (action) {
+            case MotionEvent.ACTION_DOWN:
+                handleActionDown(event.getX(), event.getY());
+                break;
+            case MotionEvent.ACTION_MOVE:
+                handleActionMove(event.getX(), event.getY());
+                break;
+            case MotionEvent.ACTION_UP:
+                handleActionUp(event.getX(), event.getY());
+                break;
+        }
+        return true;
+    }
+
+    private void handleActionUp(float x, float y) {
+        isActionMove = false;
+        isActionUp = true;
+        isActionDown = false;
+    }
+
+    private void handleActionMove(float x, float y) {
+        isActionMove = true;
+        Point point = checkSelectPoint(x, y);
+        if (point != null) {
+            addSelectedPoint(point);
+        }
+    }
+
+    private void handleActionDown(float x, float y) {
+        isActionMove = false;
+        isActionDown = true;
+        isActionUp = false;
+
+        resetCanvas();
+        if (this.patterListener != null) {
+            this.patterListener.onStart();
+        }
+
+        Point point = checkSelectPoint(x, y);
+        if (point != null) {
+            addSelectedPoint(point);
+        }
+    }
+
+    private void resetCanvas() {
+        selectPoints.clear();
+        initPoints();
+        this.postInvalidate();
+    }
+
+    private void addSelectedPoint(Point point) {
+        if (!selectPoints.contains(point)) {
+            point.setStatus(Point.POINT_PRESSED);
+            selectPoints.add(point);
+        }
+        this.postInvalidate();
+    }
+
+    private Point checkSelectPoint(float x, float y) {
+        for (int i = 0; i < points.length; i++) {
+            for (int j = 0; j < points[0].length; j++) {
+                if (LockPatternUtil.isInCircle(points[i][j], mRadius, x, y))
+                    return points[i][j];
+            }
+        }
+        return null;
     }
 
     static class Point {
-        int x, y;
-        int state;
+        private int x;// the x position of circle's center point
+        private int y;// the y position of circle's center point
+        private int row;// the cell in which row(0~n)
+        private int column;// the cell in which column(0~n)
+        private int index;// the cell value
+        private int status = 0;//default status
         final public static int POINT_NORMAL = 0,
                 POINT_PRESSED = 1,
                 POINT_ERROR = 2;
 
-        public Point(int x, int y) {
+        public Point(int x, int y, int row, int column, int index, int status) {
             this.x = x;
             this.y = y;
-            state = 0;
+            this.row = row;
+            this.column = column;
+            this.index = index;
+            this.status = status;
         }
 
+        public int getX() {
+            return x;
+        }
+
+        public void setX(int x) {
+            this.x = x;
+        }
+
+        public int getY() {
+            return y;
+        }
+
+        public void setY(int y) {
+            this.y = y;
+        }
+
+        public int getRow() {
+            return row;
+        }
+
+        public void setRow(int row) {
+            this.row = row;
+        }
+
+        public int getColumn() {
+            return column;
+        }
+
+        public void setColumn(int column) {
+            this.column = column;
+        }
+
+        public int getIndex() {
+            return index;
+        }
+
+        public void setIndex(int index) {
+            this.index = index;
+        }
+
+        public int getStatus() {
+            return status;
+        }
+
+        public void setStatus(int status) {
+            this.status = status;
+        }
+    }
+
+    public interface OnLockPattern {
+        void onStart();
+
+        void onComplete(boolean isPasswordRight);
     }
 }
